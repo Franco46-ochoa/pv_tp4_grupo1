3a1da5bb6bab77edbeefdea657ae0d7a2194d24d
fernando-eb2406 <feb-332@hotmail.com>
2025-05-21 16:53:30 -0300

    Agregado barra de busqueda

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,55 @@
-import React, { useState,useEffect } from "react";
-import ProductForm from "./Components/ProductForm";
-import ProductList from "./Components/ProductList";
-import SearchBar from "./Components/SearchBar";
-import Header from "./subcomponents/Header";
-import Footer from "./subcomponents/Footer";
-import Main from "./subcomponents/Main";
+import React, { useState, useEffect } from 'react';
+import ProductForm from './Components/ProductForm';
+import ProductList from './Components/ProductList';
+import SearchBar from './Components/SearchBar';
+import Header from './subcomponents/Header';
+import Footer from './subcomponents/Footer';
+import Main from './subcomponents/Main';
 
 function App() {
- const [Productos, setProductos] = useState([]);
- const [productoEditando, setProductoEditando] = useState(null);
+  const [Productos, setProductos] = useState([]);
+  const [productoEditando, setProductoEditando] = useState(null);
+  const [productoBuscado, setProductoBuscado] = useState([]);
+  const [descripcion, setDescripcion] = useState('');
+  const [modo, setModo] = useState('lista');
 
-
- useEffect(() => {
-    console.log("Productos actualizados:", Productos);
+  useEffect(() => {
+    console.log('Productos actualizados:', Productos);
   }, [Productos]);
   const handleDelete = (id) => {
-  const nuevosProductos = Productos.filter(p => p.Id !== id);
-  setProductos(nuevosProductos);
-};
+    const nuevosProductos = Productos.filter((p) => p.Id !== id);
+    setProductos(nuevosProductos);
+  };
+
+  const handleBuscarProducto = () => {
+    setProductoBuscado(
+      Productos.filter((producto) => producto.Descripcion === descripcion),
+    );
+    setModo('buscar');
+  };
 
   return (
-  <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
-    <Header />
+    <div
+      style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}
+    >
+      <Header />
 
-    <Main>
-      <ProductForm Productos={[Productos, setProductos]} nuevoProducto={[productoEditando, setProductoEditando]}/>
-      <SearchBar />
-      <ProductList  productos={Productos} onEdit={(producto) => setProductoEditando(producto)} onDelete={handleDelete} />
-    </Main>
+      <Main>
+        <SearchBar descripcion={[descripcion, setDescripcion]} handleBuscarProducto={handleBuscarProducto} />
 
+        {(() => {
+          switch (modo) {
+            case 'lista':
+              return <ProductList productos={Productos} onEdit={(producto) => setProductoEditando(producto)} onDelete={handleDelete} />;
+            case 'buscar':
+              return <ProductList productos={productoBuscado} onEdit={(producto) => setProductoEditando(producto)} onDelete={handleDelete} />;
+          }
+        })()}
 
-    <Footer />
-  </div>
-);
-
+        <ProductForm Productos={[Productos, setProductos]} nuevoProducto={[productoEditando, setProductoEditando]} />
+      </Main>
+      <Footer />
+    </div>
+  );
 }
 export default App;
--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,4 +1,15 @@
-function SearchBar (){
+import React from 'react'
 
+function SearchBar(props) {
+  const [descripcion, setDescripcion] = props.descripcion;
+  /* const [modo, setModo] = props.modo; */
+
+  return (
+    <div>
+      <input type="text" placeholder="Buscar producto..." value={descripcion} onChange={(e) => setDescripcion(e.target.value)} />
+      <button onClick={props.handleBuscarProducto}>Buscar</button>
+    </div>
+  )
 }
+
 export default SearchBar
